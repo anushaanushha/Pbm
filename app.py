6c27859a826eca48d2dcd15beb3d1988b0b3be8e
anushaanushha <anushaanushha@gmail.com>
2025-08-25 13:09:30 +0530

    updated change

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
 import streamlit as st
 
-# App Configuration
+
 st.set_page_config(page_title="PBM Optimization", page_icon="💊", layout="centered")
 
-# Centered Title
+
 st.markdown("<h1 style='text-align:center;'>PBM Optimization</h1>", unsafe_allow_html=True)
 st.write("---")
 
-# Dropdown for navigation
+
 option = st.selectbox(
     "Select Option",
     [
         "Choose an option",
         "Real Time Formulary Impact Analysis",
         "Therapeutic Equivalence Optimization",
-        "Drug Utilization Trend Prediction",
-        "Cost Per Member Per Month Tracking"
+        "Drug Utilization Trend Prediction"
+       
     ]
 )
 

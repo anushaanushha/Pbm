c5e34841eed6508a4f7c384d7e6438a72f6967c1
anushaanushha <anushaanushha@gmail.com>
2025-08-31 15:28:02 +0530

    deleted env

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import base64
 
 st.set_page_config(page_title="PBM Optimization", page_icon="💊", layout="centered")
 
-# Function to set background from local file
+
 def set_background(image_file):
     with open(image_file, "rb") as f:
         encoded = base64.b64encode(f.read()).decode()
@@ -27,10 +27,10 @@ def set_background(image_file):
     """
     st.markdown(bg_img, unsafe_allow_html=True)
 
-# Call background setter
-set_background("data/frontpage.jpg")  # change extension if .png
 
-# Page Title
+set_background("data/frontpage.jpg")  
+
+
 st.markdown("<h1 style='text-align:center; color:black;'>💊 PBM Optimization</h1>", unsafe_allow_html=True)
 st.write("---")
 
@@ -41,7 +41,8 @@ option = st.selectbox(
         "Formulary Impact Analysis",
         "Therapeutic Equivalence Optimization",
         "Drug Utilization Trend Prediction",
-        "Real Time Formulary Impact"
+        "Real Time Formulary Impact",
+        "Cost per member per month"
     ]
 )
 
@@ -53,3 +54,5 @@ elif option == "Drug Utilization Trend Prediction":
     st.switch_page("pages/trendprediction.py")
 elif option == "Real Time Formulary Impact":
     st.switch_page("pages/real_time.py")
+elif option == "Cost per member per month":
+    st.switch_page("pages/cost.py")
